0cf3710d9dd46643b06da146e1c3d9b6ea420861
Mohammed Mubashir <mohammedmubashir204@gmail.com>
2022-12-30 21:20:10 +0530

    -Change swift list to scroll view to lazy stack and foreach -hide navigation bar title -use text element for recipe text view

--- Recipe App/Views/RecipeListView.swift
+++ Recipe App/Views/RecipeListView.swift
@@ -18,36 +18,51 @@ struct RecipeListView: View {
         
         NavigationView {
             
-            List(model.recipes) {
-                dish in
+            VStack(alignment: .leading){
+                Text("All Recipes")
+                    .bold()
+                    .padding(.top,40)
+                    .font(.largeTitle)
                 
-                NavigationLink {
-                    RecipeDetaileView(recipe: dish)
-                } label: {
-                    
-                    //MARK: Row item
-                    
-                    HStack(spacing:20.0) {
-                        
-                        Image(dish.image)
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 50,height: 50,alignment: .center)
-                            .clipped()
-                            .cornerRadius(5)
-                        
-                        Text(dish.name)
+                ScrollView{
+                    LazyVStack(alignment: .leading){
+                        ForEach(model.recipes) {
+                            dish in
+                            
+                            NavigationLink {
+                                RecipeDetaileView(recipe: dish)
+                            } label: {
+                                
+                                //MARK: Row item
+                                
+                                HStack(spacing:20.0) {
+                                    
+                                    Image(dish.image)
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: 50,height: 50,alignment: .center)
+                                        .clipped()
+                                        .cornerRadius(5)
+                                    
+                                    Text(dish.name)
+                                        .foregroundColor(.black)
+                                }
+                            }
+                        }
                     }
+                    
                 }
+                
             }
-            .navigationBarTitle("All Recipes")
+            .navigationBarHidden(true)
+            .padding(.leading)
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        RecipeListView()
+        RecipeListView().environmentObject(RecipeModel())
     }
 }
 
